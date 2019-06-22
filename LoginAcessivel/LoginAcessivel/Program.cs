0426f76bda2ce8f7f3f86f656dbf031291153972
Jeckson Victor <jecksonvictor@gmail.com>
2019-06-22 14:48:57 -0300

    Encontrar usuário
    Encontra o nome do usuário logado no sistema e verifica se há alguma preferência de usuário salva para ele.

---
+++ LoginAcessivel/LoginAcessivel/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginAcessivel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string preferenciasDoUsuario = encontrarPreferenciasDeUsuario();
+            if (preferenciasDoUsuario != null)
+            {
+               
+            }
+            
+            //else:TERMINATE
+            
+
+        }
+
+        static string encontrarPreferenciasDeUsuario()
+        {
+            string nomeDeUsuario = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
+            string[] usuarios = System.IO.File.ReadAllLines(@"C:\Users\jecks\Documents\users.txt");
+            //string[] usuarios = System.IO.File.ReadAllLines(@"C:\users.txt");
+            foreach (string usuario in usuarios)
+            {
+                if (usuario.Contains(nomeDeUsuario))
+                {
+                    return usuario;
+                }
+            }
+            return null;
+        }
+
+        static void carregarPreferenciasDeUsuario(string usuario)
+        {
+
+        }
+    }
+}
