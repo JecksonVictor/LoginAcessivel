e69206b27131e10fddfd62286c4a32394d0ba7ac
Jeckson Victor <jecksonvictor@gmail.com>
2019-06-24 10:36:35 -0300

    Testes de ativação do alto contraste
    Vários códigos de tentativas frustradas de ativação do alto contraste por diversos meios diferentes.

--- LoginAcessivel/LoginAcessivel/Program.cs
+++ LoginAcessivel/LoginAcessivel/Program.cs
@@ -1,9 +1,14 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsInput;
+using WindowsInput.Native;
 
 namespace LoginAcessivel
 {
@@ -58,7 +63,48 @@ namespace LoginAcessivel
         {
             RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Control Panel\Accessibility\HighContrast", true);
             key.SetValue("High Contrast Scheme", value);
-            System.Windows.Forms.SendKeys.SendWait("(%+{PRTSC})");
+            key.SetValue("Flags", 1);
+            key.SetValue("LastUpdatedThemeId", 3);
+
+            RegistryKey ke2 = Registry.CurrentUser.OpenSubKey(@"Control Panel\Appearance", true);
+            ke2.SetValue("Current", "@themeui.dll, -852");
+            ke2.SetValue("NewCurrent", "@themeui.dll,-852");
+
+            //kill explorer
+            //foreach(var process in Process.GetProcessesByName("Windows Explorer")){
+            //Console.WriteLine("Matando " + process.ToString());
+            //process.Kill();
+            //}
+           // Process processo = new Process();
+
+           // processo.StartInfo = new ProcessStartInfo("taskkill", "/F /IM explorer.exe");
+
+           // processo.StartInfo.WindowStyle = ProcessWindowStyle.Normal;
+
+           // processo.Start();
+
+            //processo.WaitForExit();
+            //Process.Start("explorer.exe");
+
+            // System.Windows.Forms.SendKeys.SendWait("(%+{PRTSC})");
+
+            //InputSimulator inputSimulator = new InputSimulator();
+            //inputSimulator.Keyboard.KeyPress();
+            //inputSimulator.Keyboard.ModifiedKeyStroke(new[] { VirtualKeyCode.LMENU, VirtualKeyCode.LSHIFT, VirtualKeyCode.SNAPSHOT }, null);
+
+            //inputSimulator.Keyboard.ModifiedKeyStroke(new[] { VirtualKeyCode.SHIFT, VirtualKeyCode.MENU, VirtualKeyCode.SNAPSHOT}, null);
+
+            //inputSimulator.Keyboard.KeyDown(VirtualKeyCode.LSHIFT);
+            //inputSimulator.Keyboard.KeyDown(VirtualKeyCode.LMENU);
+            //inputSimulator.Keyboard.KeyPress(VirtualKeyCode.SNAPSHOT); 
+            //inputSimulator.Keyboard.KeyUp(VirtualKeyCode.LSHIFT);
+            //inputSimulator.Keyboard.KeyUp(VirtualKeyCode.LMENU);
+
+
+            //inputSimulator.Keyboard.ModifiedKeyStroke(new[] { VirtualKeyCode.LWIN, VirtualKeyCode.CONTROL, VirtualKeyCode.VK_C}, null);
+
+            //Console.WriteLine("Teclou");
+            //Thread.Sleep(5000);
         }
     }
 }
